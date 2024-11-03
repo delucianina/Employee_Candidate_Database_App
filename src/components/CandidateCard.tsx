b36460a470a70d1a04fd57672b5de56accafb6ca
delucianina <delucianina@gmail.com>
2024-11-03 09:26:51 -0500

    adding some notes

---
+++ src/components/CandidateCard.tsx
@@ -0,0 +1,10 @@
+// will build a card for viewing a candidate 
+// function that takes in an object or array of a user and displays that info 
+
+function CandidateCard() {
+    return {
+        
+    }
+}
+
+export default CandidateCard;
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -5,26 +5,31 @@ const CandidateSearch = () => {
 
   // ------- COPILOT HELPED -------
   // SINCE I KEEP FORGETTING:
-  // USESTATE IS A REACT FUNCTION THAT KEEPS TRACK OF THINGS THAT CAN CHANGE 
-  // RANDUSER IS THE VARIABLE 
-  // SETRANDUSER IS THE FUNCTION THAT CHANGES THE VALUE 
+  // useState IS A REACT FUNCTION THAT KEEPS TRACK OF THINGS THAT CAN CHANGE 
+  // randUser IS THE VARIABLE 
+  // setRandUser IS THE FUNCTION THAT CHANGES THE VALUE 
   const [randUser, setRandUser] = useState<string | null>(null);
+
 
   useEffect(() => {
     const fetchUser = async () => {
+      // Grabs the user info, returns 30 profiles as an array of objects 
       const user = await searchGithub();
       // CONSOLE LOG AS A TEST
       console.log(user);
 
 
-
-      setRandUser(user.login); 
+      // Not sure why copilot injected this... 
+      // I think this is setting randUser to the value of user, which would be 30 users? It's returning undefined 
+      setRandUser(user); 
       // Assuming 'login' is a string property of the user object
-      const userDetails = await searchGithubUser(user.login);
+      const userDetails = await searchGithubUser(user);
       console.log(userDetails);
     };
     fetchUser();
   }, []);
+
+
 
   return (
     <div>
