c5d968b2b5f6b035864953fff3aa9aea177164d4
delucianina <delucianina@gmail.com>
2024-11-03 10:47:40 -0500

    added some more comments, planning some of my code

--- src/components/CandidateCard.tsx
+++ src/components/CandidateCard.tsx
@@ -2,6 +2,18 @@
 // function that takes in an object or array of a user and displays that info 
 
 function CandidateCard() {
+
+    // Write a function that intakes a potential employee's info as an object 
+    // Display that info on screen in a way that looks like the provided mockup
+    
+    // This should include: 
+        // Profile avatar 
+        // Login 
+        // Location
+        // Email 
+        // Company
+        // Bio  
+
     return {
         
     }
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -4,6 +4,7 @@ import { searchGithub, searchGithubUser } from '../api/API';
 const CandidateSearch = () => {
 
   // ------- COPILOT HELPED -------
+
   // SINCE I KEEP FORGETTING:
   // useState IS A REACT FUNCTION THAT KEEPS TRACK OF THINGS THAT CAN CHANGE 
   // randUser IS THE VARIABLE 
@@ -20,21 +21,32 @@ const CandidateSearch = () => {
 
 
       // Not sure why copilot injected this... 
-      // I think this is setting randUser to the value of user, which would be 30 users? It's returning undefined 
+      // I think this is setting randUser to the value of user, which would be 30 github profiles? It's returning undefined 
       setRandUser(user); 
-      // Assuming 'login' is a string property of the user object
       const userDetails = await searchGithubUser(user);
       console.log(userDetails);
     };
+
+
     fetchUser();
+
   }, []);
 
+  // ---------- END OF COPILOT PULLING LOTS OF WEIGHT ------------ 
+  // IT JUST CONFUSED ME MORE, TO BE HONEST  
 
 
   return (
     <div>
       <h1>CandidateSearch</h1>
-      <p>Random User: {randUser}</p>
+      {/* Include CandidateCard here when it's finished */}
+      <p>Random User: </p>
+      {/* Will display randUser here 
+          
+          Are we supposed to cycle through the 30 github profiles one by one and allow the user to save or reject them?
+          If they save a candidate, add it to local memory and then show any profiles saved to local memory on the Potential Candidates page?
+          
+          I'm operating under the assumption this is the assignment, but it wasn't totally clear to me or any of my classmates I asked */}
     </div>
   );
 
